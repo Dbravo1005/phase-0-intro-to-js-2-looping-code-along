12e77640f4fe41d9d96f60a8cf27ca93043d9a29
Damian Bravo <Dbravo1005@gmail.com>
2023-03-22 23:21:56 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(array, event){
+    const resultArray = [];
+    for (let i = 0; i < array.length; i++) {
+    resultArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return resultArray
+}
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], "surprise");
+
+//function countDown(10){
+  // let i = 0; while(i < 1); 
+   //const start = 13
+//}
+//console.log(countDown);
+
+function countDown(number){
+    while (number >= 0 ){
+    console.log(number--);
+    }
+}
